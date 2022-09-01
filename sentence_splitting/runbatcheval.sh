d4f879a1dc2514866b1ea90d6804e30892629d5c
Marta Bañón <mbanon@prompsit.com>
2022-09-01 12:30:52 +0200

    Automatic benchmark, added loomchild

--- sentence_splitting/runbatcheval.sh
+++ sentence_splitting/runbatcheval.sh
@@ -1,6 +1,10 @@
 LC=$1
 TOOL=$2
 
+#Edit the vars below to adapt this script to your environment
+PYTHON=python3.10
+SEGMENT_TARGET_PATH=/home/mbanon/segment/segment-ui/target/
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 
 PREFIX=UD
 case $LC in
@@ -101,19 +105,54 @@ case $LC in
 		LN=Turkish
 		;;
 	uk)
-		LN)Ukrainian
+		LN=Ukrainian
 		;;	
 					
 esac
-	
 
+mkdir -p outfiles/
+GOLD=testsets/$PREFIX"_"$LN.dataset.gold;
+
+echo "#################################"
+echo $LC "("$LN")"
+echo "Gold standard:" $PREFIX
+echo "Tool: " $TOOL
+echo "#################################"
+	
 for FLAVOUR in none all mixed
 do
-	echo $LN
-	echo $FLAVOUR
-	echo $PREFIX
-done
 
+        echo "Testset:" $FLAVOUR
+
+        case $TOOL in
+                loomchild)
+                        for RULES in OmegaT NonAggressive PTDR language_tools.segment
+                        do
+                                echo "Loomchild SRX rules: " $RULES
+                                OUTFILE=outfiles/$PREFIX"_"$LC"_"$RULES.$FLAVOUR.out
+                                time java -cp $SEGMENT_TARGET_PATH/segment-ui-2.0.4-SNAPSHOT.jar:$SEGMENT_TARGET_PATH/segment-2.0.4-SNAPSHOT/lib/* net.loomchild.segment.ui.console.Segment -l $LC -i $GOLD -o $OUTFILE -s $SEGMENT_TARGET_PATH/../../srx/$RULES.srx
+                                $PYTHON  segmenteval.py $GOLD $OUTFILE
+                                echo "============================="
+                        done
+                        ;;
+                moses)  
+                        ;;
+                ulysses)
+                        ;;
+                nltk)   
+                        ;;
+                ersatz) 
+                        ;;
+                pymoses)
+                        ;;
+                pyloomchild)
+                        ;;
+                *)
+                        echo "Unsupported tool"
+                        exit 1
+                        ;;
+        esac
+done
 
 #gold=testsets/$prefix"_"$LN.dataset.gold; testset=testsets/$prefix"_"$LN".dataset".$flavour; outfile=outfiles/$prefix"_"$LN"_"$flavour"_nltk.out";time python3.10 nltk_segmenter.py $LN $LC $testset  $outfile && python3.10 segmenteval.py  $gold $outfile
 #LC=is; LN=Icelandic; prefix=UD; flavour=all; gold=testsets/$prefix"_"$LN.dataset.gold; testset=testsets/$prefix"_"$LN".dataset".$flavour; outfile=outfiles/$prefix"_"$LN"_"$flavour"_nltk.out";\
